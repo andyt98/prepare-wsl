07eba0a6dd87504df3a959a03bb0d251e836bb2f
andyt98 <90194383+andyt98@users.noreply.github.com>
2022-12-08 19:39:32 +0200

    Update win-install-software.ps1

--- win-install-software.ps1
+++ win-install-software.ps1
@@ -1,5 +1,5 @@
 $packages = @("Google.Chrome", "Google.Drive", "Git.Git", "Microsoft.VisualStudioCode", "Windows Subsystem for Linux",
-    "AlmaLinux 9", "JetBrains.Toolbox", "qBittorrent.qBittorrent" "RARLab.WinRAR")
+    "AlmaLinux 9", "JetBrains.Toolbox", "qBittorrent.qBittorrent" "RARLab.WinRAR" "7zip.7zip")
 
 foreach ($package in $packages) {
     winget install --silent $package 
